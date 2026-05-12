ad6d3041129c54dd3d5e3a8b3a83c3355de7ed23
Cao hanqing <caohanqing@rangming.top>
2026-05-13 01:12:59 +0800

    chore(repo): add .pytest_cache to gitignore and fix setup.py metadata

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setup(
     name="nanoclaw",
     version="1.0.0",
     packages=find_packages(),
-    py_modules=["cli"], 
     install_requires=parse_requirements('requirements.txt'),
     entry_points={
         "console_scripts": [
